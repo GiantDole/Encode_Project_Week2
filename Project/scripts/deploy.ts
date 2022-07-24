fa485dd2f816eb5e4b276829f9192ecbd5980290
GiantDole <adrian.koegl@hotmail.com>
2022-07-25 01:12:26 +0200

    Created deploy script

---
+++ Project/scripts/deploy.ts
@@ -0,0 +1,54 @@
+import { ethers } from "ethers";
+import "dotenv/config";
+import * as ballotJson from "../artifacts/contracts/CustomBallot.sol/CustomBallot.json";
+
+const EXPOSED_KEY =
+  "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
+
+function setupProvider() {
+  const infuraOptions = process.env.INFURA_API_KEY
+    ? process.env.INFURA_API_SECRET
+      ? {
+          projectId: process.env.INFURA_API_KEY,
+          projectSecret: process.env.INFURA_API_SECRET,
+        }
+      : process.env.INFURA_API_KEY
+    : "";
+  const options = {
+    alchemy: process.env.ALCHEMY_API_KEY,
+    infura: infuraOptions,
+  };
+  const provider = ethers.providers.getDefaultProvider("ropsten", options);
+  return provider;
+}
+
+async function main() {
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? EXPOSED_KEY);
+  console.log(`Using address ${wallet.address}`);
+  const provider = setupProvider();
+  const signer = wallet.connect(provider);
+  const balanceBN = await signer.getBalance();
+  const balance = Number(ethers.utils.formatEther(balanceBN));
+  console.log(`Wallet balance ${balance}`);
+  if (balance < 0.01) {
+    throw new Error("Not enough ether");
+  }
+  console.log("Deploying Ballot contract");
+  const ballotFactory = new ethers.ContractFactory(
+    ballotJson.abi,
+    ballotJson.bytecode,
+    signer
+  );
+  const ballotContract = await ballotFactory.deploy();
+  console.log("Awaiting confirmations");
+  await ballotContract.deployed();
+  console.log("Completed");
+  console.log(`Contract deployed at ${ballotContract.address}`);
+  const mintTx = await ballotContract.mint(wallet.address, 100);
+  await mintTx.wait();
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
